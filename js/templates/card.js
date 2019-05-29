b895e04b2ceb10a6748f506017e14122dd3986e5
Pedro Almir <petrus.cc@gmail.com>
2019-05-29 13:38:18 -0300

    Ajuste no icone do card.

--- js/templates/card.js
+++ js/templates/card.js
@@ -1,7 +1,7 @@
 var cardTemplate = '<div class="card" style="margin: 5px 10px;"><div class="card-header" id="heading{{=it.cardID}}"><h2 class="mb-0">';
       cardTemplate += '<button class="btn btn-link collapsed" style="box-shadow: none; color: #064f9e; width: 98%; text-align: left;" type="button" data-toggle="collapse" data-target="#collapse{{=it.cardID}}" aria-expanded="false" aria-controls="collapse{{=it.cardID}}">';
       cardTemplate += '{{=it.cardTitle}}';
-      cardTemplate += '</button><i class="fas fa-arrow-circle-down" style="color: #064f9e;font-size: 22px"></i></h2></div>';
+      cardTemplate += '</button><i class="fas fa-angle-down" style="color: #064f9e;font-size: 22px"></i></h2></div>';
       cardTemplate += '<div id="collapse{{=it.cardID}}" class="collapse" aria-labelledby="heading{{=it.cardID}}"><div class="card-body">';
       cardTemplate += '<div class="row" style="padding: 0px 20px;"><p class="text-break">{{=it.sHint}}</p></div>';
       cardTemplate += '{{=it.cardContent}}';
